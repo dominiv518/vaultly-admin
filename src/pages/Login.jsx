2d98ee47c2feaab2d98793ab5ef449ae44dc557d
dominiv518 <dominiv518@gmail.com>
2026-08-05 10:52:28 +0100

    Fix back-to-login button arrow direction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -363,7 +363,7 @@ export default function Login() {
                   setError('');    // clear any error message
                 }}
               >
-                ΓåÉ Back to login
+              Back to login &larr;
               </button>
             </form>
           )}
